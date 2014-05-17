0358138c9ba06ecfe3cef8485b653856467b4f43
aki123 <mio_419@hotmail.com>
2014-05-17 16:30:22 +0900

    hello.c

--- hello.c
+++ hello.c
@@ -1,9 +1,10 @@
-/*  はじめてのCプログラミング*/
 #include <stdio.h>
 
 int main()
 {
-    printf("hello World C Language\n");
+    int i;
+    for (i = 0; i < 10; i++) {
+        printf("Hello World\n");
+    }
     return 0;
 }
-
