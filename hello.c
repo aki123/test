56b60d921a05114690de814a428673c1ba05c7f5
aki123 <mio_419@hotmail.com>
2014-05-31 13:34:57 +0900

    update hello.c

--- hello.c
+++ hello.c
@@ -2,9 +2,15 @@
 
 int main(){
 
-    int i;
-    for (i = 0; i < 10; i++) {
-        printf("hello world\n");
-	}
+    printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
     return 0;
 }
