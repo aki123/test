f527b2b348105914632c4e41202774821995001a
aki123 <mio_419@hotmail.com>
2014-05-31 13:37:09 +0900

    update hello.c

--- hello.c
+++ hello.c
@@ -3,14 +3,14 @@
 int main(){
 
     printf("Hello World\n");
-	printf("Hello World\n");
-	printf("Hello World\n");
-	printf("Hello World\n");
-	printf("Hello World\n");
-	printf("Hello World\n");
-	printf("Hello World\n");
-	printf("Hello World\n");
-	printf("Hello World\n");
-	printf("Hello World\n");
+    printf("Hello World\n");
+    printf("Hello World\n");
+    printf("Hello World\n");
+    printf("Hello World\n");
+    printf("Hello World\n");
+    printf("Hello World\n");
+    printf("Hello World\n");
+    printf("Hello World\n");
+    printf("Hello World\n");
     return 0;
 }
